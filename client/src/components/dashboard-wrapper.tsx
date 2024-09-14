24a58fef9d835c2fa38dab6a5b09a68293973d31
Azher Uddin <azher305uddin@gmail.com>
2024-09-14 13:44:47 +0600

    feat: navbar

--- client/src/components/dashboard-wrapper.tsx
+++ client/src/components/dashboard-wrapper.tsx
@@ -1,9 +1,12 @@
+import Navbar from "./Navbar";
+
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
       <main className="dark:bg-dark-bg md:pl64 w-full flex-1 flex-col bg-gray-50">
         {/* navbar */}
+        <Navbar />
         {children}
       </main>
     </div>
