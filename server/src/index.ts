36d0e8176489c1a391ca14093b932f1f63411b1f
Azher Uddin <azher305uddin@gmail.com>
2024-09-23 02:23:16 +0600

    feat: server init

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const PORT = process.env.PORT || 5000;
+
+// ROUTE IMPORTS
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ROUTES
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+// LISTEN SERVER
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
